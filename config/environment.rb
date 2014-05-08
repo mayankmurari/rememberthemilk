36fe6710df66004b248776ab7914ba9d4910230b
Mayank Murari <mayank.murari@gmail.com>
2014-05-08 12:46:13 +0530

    Heroku seetings

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rememberthemilk::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "25",
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN']
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
 
 
