04216435d866c975e6b54334716244bc990588c0
Mayank Murari <mayank.murari@gmail.com>
2014-05-08 12:38:37 +0530

    changes

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rememberthemilk::Application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
+
 
